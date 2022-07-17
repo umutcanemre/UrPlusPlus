4cd6c0eaf596e0a047b8b94226b5d8616991a778
AntiChange <chr1sf4ng@gmail.com>
2022-07-17 14:59:31 -0400

    Add binomial dist. dice roll

--- src/gamestate.cc
+++ src/gamestate.cc
@@ -1,10 +1,14 @@
 #include <memory>
 #include "gamestate.h"
 #include "board.h"
+#include <random>
 
 void GameState::rollDice() {
-    flexdiceroll = 4;
-    diceroll = 4;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::binomial_distribution<> d(4, 0.5);    
+    flexdiceroll = d(gen);
+    diceroll = d(gen);
 }
 
 const std::vector<std::vector<Tile*>>& GameState::getBoard() const {
